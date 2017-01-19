96e5a4a0d27d9f36e0ef624aef8b46b351d65e64
k2600 <k2600@OHJE-330-WS-32>
2017-01-19 11:25:36 +0200

    Counts numbers

--- Teht09/Program.cs
+++ Teht09/Program.cs
@@ -11,15 +11,15 @@ namespace Teht09
         static void Main(string[] args)
         {
             Console.WriteLine("You can give me some random numbers and I count for you.\nIf you want to stop give zero(0) and press enter.");
-            int i;
+            int i, j = 0 ;
             do
             {
                 i = int.Parse(Console.ReadLine());
-                int j = 0;
-                j = i + j;
-                Console.WriteLine("Counting : {0}", j);
+                j = i + j ;
+                //Console.WriteLine("Counting : {0}", j);
             }
             while (i != 0);
+            Console.WriteLine("The final is {0}", j);
                         
  
         }
