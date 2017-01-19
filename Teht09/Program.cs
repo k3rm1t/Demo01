d26bb899ef9d7662830e9771401e6b549f828bf1
k2600 <k2600@OHJE-330-WS-32>
2017-01-19 11:15:40 +0200

    somtehing new

---
+++ Teht09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You can give me some random numbers and I count for you.\nIf you want to stop give zero(0) and press enter.");
+            int i;
+            do
+            {
+                i = int.Parse(Console.ReadLine());
+                int j = 0;
+                j = i + j;
+                Console.WriteLine("Counting : {0}", j);
+            }
+            while (i != 0);
+                        
+ 
+        }
+    }
+}
